86a7e2b1593c8c9b57d2529c22d40db140c8e1e9
Dave <davefunk135@gmail.com>
2017-12-29 22:47:03 -0800

    added loop to print buttons, played with api for a while

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
-//javascript, jQuery
-$( document ).ready(function() {
-    console.log( "ready!" );
-    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-// xhr.done(function(data) { console.log("success got data", data); });
+$(document).ready(function () {
+    var overwatchArray = ["Overwatch", "Tracer", "Widowmaker", "Mei", "Bastion", "Doomfist", "Mercy", "Winston", "Sombra", "Hanzo", "Zenyatta", "Genji", "Mcree", "Pharah", "Reaper", "Soldier", "Junkrat", "Torbjorn", "DVA", "Orisa", "Reinhardt", "Roadhog", "Zarya", "Ana Overwatch", "Lucio", "Moira", "Symmetra"];
+    for (var i = 0; i < overwatchArray.length; i++) {
+        var overwatchButtons = $('<button>');
+        overwatchButtons.addClass("owbutton");
+        overwatchButtons.text(overwatchArray[i]);
+        $('#querybuttons').append(overwatchButtons);
+    }
+    //Example Query
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=5");
+    xhr.done(function (data) {
+        console.log("success got data", data);
+    });
 });
