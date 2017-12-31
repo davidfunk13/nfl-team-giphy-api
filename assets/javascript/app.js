49e1029f99d6458787a168dd93cfc5cfde19f0cf
Dave <davefunk135@gmail.com>
2017-12-30 17:09:56 -0800

    going over old class materials practicing activities, added best practice ajax call, and a placeholder variable for a data attribute for the buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
     var overwatchArray = ["Overwatch", "Tracer", "Widowmaker", "Mei", "Bastion", "Doomfist", "Mercy", "Winston", "Sombra", "Hanzo", "Zenyatta", "Genji", "Mcree", "Pharah", "Reaper", "Soldier", "Junkrat", "Torbjorn", "DVA", "Orisa", "Reinhardt", "Roadhog", "Zarya", "Ana Overwatch", "Lucio", "Moira", "Symmetra"];
-    for (var i = 0; i < overwatchArray.length; i++) {
-        var overwatchButtons = $('<button>');
-        overwatchButtons.addClass("owbutton");
-        overwatchButtons.text(overwatchArray[i]);
-        $('#querybuttons').append(overwatchButtons);
-    }
+
     $(document).on("click", ".owbutton", function () {
         var buttonText = $(this).text();
         console.log(buttonText);
 
     });
-    //Example Query (*USE HTTPS*)
-    var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=5");
-    xhr.done(function (data) {
-        console.log("success got data", data);
-    });
+
+        var hero = $(this).attr('data-hero');
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=5";
+
+        $.ajax({
+            url: queryUrl,
+            method: 'GET'
+        }).done(function (response) {
+            console.log(response);
+        });
+
+        $('#querybuttons').empty();
+
+        for (var i = 0; i < overwatchArray.length; i++) {
+            var overwatchButtons = $('<button>');
+            overwatchButtons.addClass("owbutton");
+            overwatchButtons.text(overwatchArray[i]);
+            overwatchButtons.attr("data-hero", overwatchArray[i]);
+            $('#querybuttons').append(overwatchButtons);
+        }
 });
