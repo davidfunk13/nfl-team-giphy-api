73c50c68d498980dd3d2a6c6d626e5d8ab80af83
Dave <davefunk135@gmail.com>
2017-12-30 23:56:25 -0800

    now topiuc changed to us cities and is appending to page. working on adding more results and info per click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,39 @@
 // document ready wrapper
 $(document).ready(function () {
     //array of searches to print to buttons
-    var overwatchArray = ["Overwatch", "Tracer", "Widowmaker", "Mei", "Bastion", "Doomfist", "Mercy", "Winston", "Sombra", "Hanzo", "Zenyatta", "Genji", "Mcree", "Pharah", "Reaper", "Soldier", "Junkrat", "Torbjorn", "DVA", "Orisa", "Reinhardt", "Roadhog", "Zarya", "Ana Overwatch", "Lucio", "Moira", "Symmetra"];
+    var queryButtonsDiv = $('#querybuttons')
+    var unitedStatesCities = ["Baltimore", "Atlanta", "Houston", "Detroit", "Los Angeles", "Boston", "Denver", "Miami", "Las Vegas", "New York", "Chicago", "Philadelphia", "New Orleans", "Milwaukee", "Kansas City", "Seattle", "Dallas", "Cleveland", "St. Louis"];
 //function for the click event listener
     function clickButtons() {
         //variable container for the data attribute string attached to the buttons
-        var hero = $(this).attr('data-hero');
-        console.log(hero)
+        var searchTerms = $(this).attr('data-city');
+        console.log(searchTerms)
         //url querying the api
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=5";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerms + "&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=5";
         $.ajax({
             url: queryUrl,
             method: 'GET'
         }).done(function (response) {
             console.log(response);
+            var gifUrl = response.data["0"].images.fixed_width.url;
+            console.log(gifUrl);
+            newGif = $("<img>").attr("src", gifUrl);
+            $('#results').append(newGif);
+
+$()
             // var newGif = $("<div class='newgif'>")
         });
     }
 
     $('#querybuttons').empty();
 //loop looping through the array of options, creating buttons, appending text to the buttons as well as a data attribute "hero" with a class of .owbutton
-    for (var i = 0; i < overwatchArray.length; i++) {
+    for (var i = 0; i < unitedStatesCities.length; i++) {
         var buttonText = $(this).text();
-        var overwatchButtons = $('<button>');
-        overwatchButtons.addClass("owbutton");
-        overwatchButtons.text(overwatchArray[i]);
-        overwatchButtons.attr("data-hero", overwatchArray[i]);
-        $('#querybuttons').append(overwatchButtons);
+        var cityButtons = $('<button>');
+        cityButtons.addClass("owbutton");
+        cityButtons.text(unitedStatesCities[i]);
+        cityButtons.attr("data-city", unitedStatesCities[i]);
+       queryButtonsDiv.append(cityButtons);
     }
     //global click event listener
     $(document).on("click", ".owbutton", clickButtons);
