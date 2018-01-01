d44309638476527d6125e656c4dffd41d1b7d6fc
Dave <davefunk135@gmail.com>
2017-12-31 16:38:30 -0800

    nfl teams now topic. adding seperate fivs per result, so ten total. also added rating. more coming.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,41 @@
-// document ready wrapper
 $(document).ready(function () {
-    //array of searches to print to buttons
-    var queryButtonsDiv = $('#querybuttons')
-    var unitedStatesCities = ["Baltimore", "Atlanta", "Houston", "Detroit", "Los Angeles", "Boston", "Denver", "Miami", "Las Vegas", "New York", "Chicago", "Philadelphia", "New Orleans", "Milwaukee", "Kansas City", "Seattle", "Dallas", "Cleveland", "St. Louis"];
-//function for the click event listener
+    var queryButtonsDiv = $('#querybuttons');
+    var nflTeams = ["Baltimore Ravens", "Atlanta Falcons", "Houston Texans", "Detroit Lions", "Los Angeles Rams", "New England Patriots", "Denver Broncos", "Miami Dolphins", "Buffalo Bills", "Chicago Bears", "Philadelphia Eagles", "New Orleans Saints", "Green Bay Packers", "Kansas City Cheifs", "Seattle Seahawks", "Dallas Cowboys", "Cleveland Browns"];
+
     function clickButtons() {
-        //variable container for the data attribute string attached to the buttons
-        var searchTerms = $(this).attr('data-city');
+        var searchTerms = $(this).attr('data-team');
         console.log(searchTerms)
-        //url querying the api
         var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerms + "&api_key=3mnOK9PA7RKmAirX3qtFBIp7D2yIy0hQ&limit=10";
         $.ajax({
             url: queryUrl,
-            method: 'GET'
-        }).done(function (response) {
+            method: "GET"
+        }).done(function(response) {
             console.log({"API Response": response});
-            console.log({"Results length": response.data});
             for (var j = 0; j < response.data.length; j++) {
+                gifContainer = $("<div class='container'>");
                 var gifUrl = response.data[j].images.fixed_width.url;
-                console.log(gifUrl);
-                newGif = $("<img>").attr("src", gifUrl);
-                $('#results').append(newGif);
+                var gifRating = response.data[j].rating;
+                ratingText = $('<p>').text(gifRating);
+                var gifTitle = response.data[j].title;
+                var gifImage = $("<img>").attr("src", gifUrl);
+                $('#results').append(gifContainer);
+                gifContainer.append(gifImage);
+                gifContainer.append("Rating: " + gifRating);
+                
             }
-
-$()
-            // var newGif = $("<div class='newgif'>")
         });
     }
-
-    $('#querybuttons').empty();
-//loop looping through the array of options, creating buttons, appending text to the buttons as well as a data attribute "hero" with a class of .owbutton
-    for (var i = 0; i < unitedStatesCities.length; i++) {
-        var buttonText = $(this).text();
-        var cityButtons = $('<button>');
-        cityButtons.addClass("owbutton");
-        cityButtons.text(unitedStatesCities[i]);
-        cityButtons.attr("data-city", unitedStatesCities[i]);
-       queryButtonsDiv.append(cityButtons);
+    function displayButtons(){
+        for (var i = 0; i < nflTeams.length; i++) {
+            var buttonText = $(this).text();
+            var teamButtons = $('<button>');
+            teamButtons.addClass("teambutton");
+            teamButtons.text(nflTeams[i]);
+            teamButtons.attr("data-team", nflTeams[i]);
+           queryButtonsDiv.append(teamButtons);
+        }
     }
+    displayButtons();
     //global click event listener
-    $(document).on("click", ".owbutton", clickButtons);
+    $(document).on("click", ".teambutton", clickButtons);
 });
